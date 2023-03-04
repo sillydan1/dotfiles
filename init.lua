4192976b89a284d4451af8e3e6cf76dced120fb4
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-03-04 10:57:19 +0100

    feat: add barbar plugin

--- init.lua
+++ init.lua
@@ -74,6 +74,9 @@ require('packer').startup(function(use)
   -- multiple visual cursors
   use 'mg979/vim-visual-multi'
 
+  -- tabs
+  use {'romgrk/barbar.nvim', requires = 'nvim-web-devicons'}
+
   -- Git related plugins
   use 'tpope/vim-fugitive'
   use 'tpope/vim-rhubarb'
@@ -639,6 +642,47 @@ local function open_nvim_tree()
   require('nvim-tree.api').tree.open()
 end
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+-- barbar configuration
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '<leader>hh', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<leader>ll', '<Cmd>BufferNext<CR>', opts)
+-- Re-order to previous/next
+map('n', '<leader><', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<leader>>', '<Cmd>BufferMoveNext<CR>', opts)
+-- Goto buffer in position...
+map('n', '<leader>1', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<leader>2', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<leader>3', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<leader>4', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<leader>5', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<leader>6', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<leader>7', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<leader>8', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<leader>9', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<leader>0', '<Cmd>BufferLast<CR>', opts)
+-- Pin/unpin buffer
+map('n', '<leader>tp', '<Cmd>BufferPin<CR>', opts)
+-- Close buffer
+map('n', '<leader>tc', '<Cmd>BufferClose<CR>', opts)
+-- Wipeout buffer
+--                 :BufferWipeout
+-- Close commands
+--                 :BufferCloseAllButCurrent
+--                 :BufferCloseAllButPinned
+--                 :BufferCloseAllButCurrentOrPinned
+--                 :BufferCloseBuffersLeft
+--                 :BufferCloseBuffersRight
+-- Magic buffer-picking mode
+map('n', '<C-p>', '<Cmd>BufferPick<CR>', opts)
+-- Sort automatically by...
+map('n', '<leader>bb', '<Cmd>BufferOrderByBufferNumber<CR>', opts)
+map('n', '<leader>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
+map('n', '<leader>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
+map('n', '<leader>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
