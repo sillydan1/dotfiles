b0fa922e36d69faa3a4479dcfbcfdf9d91ee976f
Asger Gitz-Johansen <asgj@gomspace.com>
2024-03-19 12:41:02 +0100

    fix: update nvim-dap-ui dependencies

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,7 +21,7 @@ vim.g.loaded_netrwPlugin = 1
 require("lazy").setup({
   { 'nvim-tree/nvim-tree.lua', dependencies = { 'nvim-tree/nvim-web-devicons' } },
   { 'folke/todo-comments.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
-  { "rcarriga/nvim-dap-ui", dependencies = { 'mfussenegger/nvim-dap' } },
+  { "rcarriga/nvim-dap-ui", dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' } },
   { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-fzf-native.nvim' } },
   { 'neovim/nvim-lspconfig', dependencies = { 'williamboman/mason.nvim', 'williamboman/mason-lspconfig.nvim', 'folke/neodev.nvim' } },
   { 'hrsh7th/nvim-cmp', dependencies = { 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-nvim-lsp', 'saadparwaiz1/cmp_luasnip','L3MON4D3/LuaSnip', 'rafamadriz/friendly-snippets' } },
