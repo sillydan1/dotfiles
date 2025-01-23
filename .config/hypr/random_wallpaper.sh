7cbef0af43fca6b02a36319f0f6b73627351f757
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2025-01-23 20:29:04 +0100

    fix: also look in the included wallpapers for random selection

--- .config/hypr/random_wallpaper.sh
+++ .config/hypr/random_wallpaper.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 conf_dir="$HOME/.config/hypr"
-random_pick=$(find "$HOME/Pictures/wallpapers/" -type f -name "*.png" -o -name "*.jpg" -o -name "*.JPEG" | shuf -n1)
+random_pick=$(find "$HOME/Pictures/wallpapers/" "$HOME/.config/.wallpapers/" -type f -name "*.png" -o -name "*.jpg" -o -name "*.JPEG" | shuf -n1)
 sed -e "s~<wp>~${random_pick}~g" $conf_dir/hyprpaper.template > $conf_dir/hyprpaper.conf
